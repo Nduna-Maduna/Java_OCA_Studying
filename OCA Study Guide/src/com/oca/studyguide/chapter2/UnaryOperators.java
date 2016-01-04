5432ead030ce775bf78ab65b8796a3d2859f1265
davidmartins <davids.martinz@gmail.com>
2016-01-04 16:48:41 +0000

    Java Operators

---
+++ OCA Study Guide/src/com/oca/studyguide/chapter2/UnaryOperators.java
@@ -0,0 +1,16 @@
+package com.oca.studyguide.chapter2;
+
+public class UnaryOperators {
+
+    public static void main(String[] args) {
+
+        // Apparently pre-unary and post-unary operators have the same precedence level
+
+        int x = 3;
+        int y = ++x * 5 / x-- + --x;
+
+        System.out.println("x is " + x);
+        System.out.println("y is " + y);
+    }
+
+}
