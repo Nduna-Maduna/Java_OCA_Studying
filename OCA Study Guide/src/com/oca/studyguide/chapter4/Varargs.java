eec5823e6863b5f91e29e92a817d952eec9a4a1f
davidmartins <davids.martinz@gmail.com>
2016-02-02 22:41:07 +0000

    Varargs, chapter 4

---
+++ OCA Study Guide/src/com/oca/studyguide/chapter4/Varargs.java
@@ -0,0 +1,24 @@
+package com.oca.studyguide.chapter4;
+
+public class Varargs {
+
+    public static void main(String[] args) {
+
+        walk(1);
+        walk(1, 2);
+        walk(1, 2, 3);
+        walk(1, new int[] {4, 5, 6});
+    }
+
+
+    /*
+     * Using varargs has to follow two simple rules:
+     * 1. The varargs variable has to be the last parameter in the list parameter of a method.
+     * 2. Only one vararg parameter per method is allowed.
+     */
+    public static void walk(int start, int... numbers) {
+
+        System.out.println(numbers.length);
+
+    }
+}
